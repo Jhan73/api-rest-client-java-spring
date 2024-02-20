8ac6f5ec4b3cd33ea7bec35bce369f927b6c2fdb
Jhan73 <jhan.antezana6174@gmail.com>
2024-02-20 18:25:41 -0500

    creacion de entidad

---
+++ src/main/java/com/jhan/tutorial/model/entity/Cliente.java
@@ -0,0 +1,31 @@
+package com.jhan.tutorial.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+//usando anotaciones de lombok
+@Data //genera los getter, setter, hashCode, canEqualm, toString y equals.
+      //pero tambien se puede decorar de manera individual como @setter, @getter ...
+@AllArgsConstructor //constructor con todos los argumentos
+@NoArgsConstructor //constructor vacio
+//@ToString //genera el toString
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+    @Id
+    @Column(name = "id_cliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "nombre")//en este caso no es necesario este decorador ps en la base de datos tiene el mismo nombre el campo correspondiente al atributo Cliente
+    private String nombre;
+    private String apellido;
+    private String correo;
+    @Column(name = "fecha_registro")
+    private Date fechaRegistro;
+}

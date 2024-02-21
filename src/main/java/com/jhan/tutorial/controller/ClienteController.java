7ddf5c2ff7d208c04bb0504b83be0daec97c94d4
Jhan73 <jhan.antezana6174@gmail.com>
2024-02-20 22:22:19 -0500

    implementacion de STATUS basica

--- src/main/java/com/jhan/tutorial/controller/ClienteController.java
+++ src/main/java/com/jhan/tutorial/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.jhan.tutorial.controller;
 import com.jhan.tutorial.model.entity.Cliente;
 import com.jhan.tutorial.service.ClienteInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,19 +12,23 @@ public class ClienteController {
     @Autowired
     private ClienteInterface clienteInterface;
     @PostMapping("/cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente){
         return clienteInterface.save(cliente);
     }
     @PutMapping("/cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente update(@RequestBody Cliente cliente){
         return clienteInterface.save(cliente);
     }
     @DeleteMapping("/cliente/{id}")//{id} debe coincidir con el nombre del parametro
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Integer id){
         Cliente clienteDelete = clienteInterface.findById(id);
         clienteInterface.delete(clienteDelete);
     }
     @GetMapping("/cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Cliente showById(@PathVariable Integer id){
         return clienteInterface.findById(id);
     }
